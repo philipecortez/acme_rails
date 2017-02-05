7bf4f2c194f0057edb5152f738d01a6b1eb2d045
Philipe Cortez <philipesousacortez@gmail.com>
2017-02-05 20:23:34 -0200

    add some unit tests to task_spec

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,4 +1,47 @@
 RSpec.describe Task do
-  it 'has a description'
-  it 'can be completed'
+
+  let(:user) { User.new(name: 'Philipe', email: 'philipesousacortez@gmail.com', password: 'acme123') }
+
+  let(:task_list) { TaskList.create(name: 'Tarefas do dia', user: user) }
+
+  subject { Task.create(description: "Acme global domination", task_list: task_list) }
+
+  it 'has a description' do
+    expect(subject.description).to eq("Acme global domination")
+  end
+
+  it 'default value to completed is false' do
+    expect(subject.completed?).to be_falsy
+  end
+
+  it 'can be completed' do
+    subject.completed = true
+    expect(subject.completed).to be_truthy
+  end
+
+  context 'validations' do
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+
+    it 'is invalid without a description' do
+      subject.description = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is invalid without a task list' do
+      subject.task_list = nil
+      expect(subject).to_not be_valid
+    end
+
+  end
+
+  context 'associations' do
+    it 'belongs to a task list' do
+      expect(Task.reflect_on_association(:task_list).macro).to eq(:belongs_to)
+    end
+
+  end
+
+
 end
