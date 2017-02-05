ddddbced4f73552ac539aab8be96a4b4066ee1e5
Philipe Cortez <philipesousacortez@gmail.com>
2017-02-05 21:11:16 -0200

    setting up devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   validates :email, :name, :password, presence: true
 
   has_many :task_lists
