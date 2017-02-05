69702e22daef4f0902ea06e0b66fd68ee24365b3
Philipe Cortez <philipesousacortez@gmail.com>
2017-02-05 17:18:58 -0200

    add association has many tasks to task lists and some tests

--- app/models/task_list.rb
+++ app/models/task_list.rb
@@ -3,5 +3,5 @@ class TaskList < ActiveRecord::Base
   validates :name, presence: true
 
   belongs_to :user
-
+  has_many :tasks
 end
--- spec/models/task_list_spec.rb
+++ spec/models/task_list_spec.rb
@@ -4,13 +4,18 @@ RSpec.describe TaskList, type: :model do
   
   let(:user) { User.new(name: 'Philipe', email: 'philipesousacortez@gmail.com', password: 'acme123') }
   
-  subject { TaskList.new(name: 'Tarefas do dia', public: true, user: user) }
+  subject { TaskList.new(name: 'Tarefas do dia', user: user) }
 
   it 'has a name' do
     expect(subject.name).to eq('Tarefas do dia')
   end
 
+  it 'default value for public is false' do
+    expect(subject.public).to be_falsy
+  end
+
   it 'can be public' do
+    subject.public = true
     expect(subject.public).to be_truthy
   end
 
@@ -27,6 +32,16 @@ RSpec.describe TaskList, type: :model do
     it 'is not valid without a user' do
       subject.user = nil
       expect(subject).to_not be_valid
+    end
+  end
+
+  context 'associations' do
+    it 'belongs to a user' do
+      expect(TaskList.reflect_on_association(:user).macro).to eq(:belongs_to)
+    end
+
+    it 'has many tasks' do
+      expect(TaskList.reflect_on_association(:tasks).macro).to eq(:has_many)
     end
   end
 end
