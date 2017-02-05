7d9f6de76a9259d7ae416dfe4dab5348cff2019c
Philipe Cortez <philipesousacortez@gmail.com>
2017-02-05 16:59:57 -0200

    add task list spec and model

--- app/models/task_list.rb
+++ app/models/task_list.rb
@@ -1,3 +1,7 @@
 class TaskList < ActiveRecord::Base
+  
+  validates :name, presence: true
+
   belongs_to :user
+
 end
--- spec/models/task_list_spec.rb
+++ spec/models/task_list_spec.rb
@@ -1,7 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe TaskList, type: :model do
-  it 'has a name'
-  it 'has a public'
+  
+  let(:user) { User.new(name: 'Philipe', email: 'philipesousacortez@gmail.com', password: 'acme123') }
+  
+  subject { TaskList.new(name: 'Tarefas do dia', public: true, user: user) }
 
+  it 'has a name' do
+    expect(subject.name).to eq('Tarefas do dia')
+  end
+
+  it 'can be public' do
+    expect(subject.public).to be_truthy
+  end
+
+  context 'validations' do
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid without a name' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without a user' do
+      subject.user = nil
+      expect(subject).to_not be_valid
+    end
+  end
 end
