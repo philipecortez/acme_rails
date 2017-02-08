ff714e3ac474fa7e8d66aadb5d1e539d39c2b696
Sr. Philipe <philipesousacortez@gmail.com>
2017-02-08 18:26:12 -0200

    set the devise sign_up and sign_in redirect_to to another path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,15 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  def after_sign_in_path_for(resource)
+    binding.pry
+    if resource.is_a?(User) && user_signed_in?
+      user_task_lists_path(resource)
+    else
+      super
+    end
   end
 end
