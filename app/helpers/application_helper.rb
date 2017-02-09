f95c2511647089aa1de9842335b050da15b9ee9e
Philipe Cortez <philipesousacortez@gmail.com>
2017-02-09 08:02:01 -0200

    add a helper to change icon class

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def set_class_for_attr_public(task_list)
+    if task_list.public?
+      'fa fa-check-square-o fa-2x'
+    else
+      'fa fa-square-o fa-2x'
+    end
+  end
 end
