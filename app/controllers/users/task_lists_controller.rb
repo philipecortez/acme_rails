c7f1d0e2bcf026f4a998a1269ed384a8e3b5eab9
Philipe Cortez <philipesousacortez@gmail.com>
2017-02-09 08:04:09 -0200

    make destroy, update, new and create task_list controller actions

--- app/controllers/users/task_lists_controller.rb
+++ app/controllers/users/task_lists_controller.rb
@@ -1,21 +1,32 @@
 class Users::TaskListsController < ApplicationController
   def index
-    binding.pry
     @task_lists = TaskList.from_user(params[:user_id])
   end
 
   def create
+    TaskList.create(params[:task_list].permit!)
+    redirect_to user_task_lists_path(params[:user_id])
   end
 
   def update
+    task_list = TaskList.find(params[:id])
+    task_list.update_attributes(params[:task_list].permit!)
+    respond_to do |format|
+      format.html { redirect_to user_task_lists_path(params[:user_id]) }
+      format.json { render task_list.to_json }
+    end
+
   end
 
   def new
+    @task_list = TaskList.new
   end
 
   def edit
   end
 
   def destroy
+    @id = params[:id]
+    TaskList.destroy(@id)
   end
 end
