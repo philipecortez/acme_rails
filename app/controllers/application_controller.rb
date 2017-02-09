77e5759e2375b7a90b0d8f9400e9296df61d6094
Philipe Cortez <philipesousacortez@gmail.com>
2017-02-09 07:59:04 -0200

    add a layout to devise pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  layout :layout_by_resource
 
   protected
+
+  def layout_by_resource
+    if devise_controller?
+      'devise'
+    else
+      'application'
+    end
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
   def after_sign_in_path_for(resource)
-    binding.pry
     if resource.is_a?(User) && user_signed_in?
       user_task_lists_path(resource)
     else
