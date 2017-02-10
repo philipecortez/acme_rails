e012904e99819834227db3a36f3aade1e3b266f3
Philipe Cortez <philipesousacortez@gmail.com>
2017-02-10 17:10:24 -0200

    implement favorite task lists relations in user and task list models and make favorite and unfavorite in task lists

--- app/models/task_list.rb
+++ app/models/task_list.rb
@@ -5,6 +5,18 @@ class TaskList < ActiveRecord::Base
   belongs_to :user
   has_many :tasks
 
+  has_many :favorite_task_lists
+  has_many :users_who_favored, through: :favorite_task_lists, source: :user
+
+
   scope :from_user, lambda { |user| where(user: user) }
   scope :publics, lambda { where(public: true)}
+
+  def favorite(user_id)
+    FavoriteTaskList.where(user_id: user_id, task_list_id: self.id).first_or_create
+  end
+
+  def unfavorite(user_id)
+    FavoriteTaskList.where(user_id: user_id, task_list_id: self.id).destroy_all
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ActiveRecord::Base
   validates :email, :name, :password, presence: true
 
   has_many :task_lists
+
+  has_many :favorite_task_lists
+  has_many :favorited_task_lists, through: :favorite_task_lists, source: :task_list
 end
